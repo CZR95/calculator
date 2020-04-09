ccac015663451649dc3f8ce0764ca5ac8992fa8a
eric chong <rong002009@hotmail.com>
2020-04-10 01:00:26 +0800

    Updates

---
+++ precache-manifest.aab9eb26fd8d705bab30f794b81efaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21522ad2cc7e70f9d74c293a25645142",
+    "url": "/CZR95/calculator/index.html"
+  },
+  {
+    "revision": "eda6302beb16af749727",
+    "url": "/CZR95/calculator/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "eda6302beb16af749727",
+    "url": "/CZR95/calculator/static/js/2.a6bc0e01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CZR95/calculator/static/js/2.a6bc0e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5768488eac1a832d2f52",
+    "url": "/CZR95/calculator/static/js/main.c87ef42f.chunk.js"
+  },
+  {
+    "revision": "3f18f1d2c8390337dfa8",
+    "url": "/CZR95/calculator/static/js/runtime-main.69ba4d55.js"
+  }
+]);
